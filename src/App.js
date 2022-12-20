7d8107ef2a503dd58399e6eb80261d622c5a11e6
jozaghi <ar.jozaghi@hotmail.com>
2022-12-19 16:09:44 -0800

    add CD

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import firebase from "./FirebaseConfig";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="p-10">
+     <div className="text-2xl font-bold text-center p-5">Firebase</div>
     </div>
   );
 }
